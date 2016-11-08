14c6455b8c3e73739b273a23b127a0436f3413bf
YidingW <wangyiding.chinese@gmail.com>
2016-11-08 22:47:59 +0800

    打架脚手架 * react相关lib * Container和Component

---
+++ app/components/Player/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class List extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <h1>Component</h1>
+    );
+  }
+}
